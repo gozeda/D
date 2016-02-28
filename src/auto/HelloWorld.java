06126064de72926927946e6d31eef84f6f4169e7
yinzheda <yinzheda@gmail.com>
2016-02-28 17:23:07 +0800

    renew HelloWorld and add TestEnv

--- src/auto/HelloWorld.java
+++ src/auto/HelloWorld.java
@@ -1,4 +1,4 @@
-﻿package auto;
+package auto;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
@@ -30,7 +30,7 @@ public class HelloWorld {
 
 		String txtExpected = "百度一下";
 		String txtActual = driver.findElement(By.xpath("//input[@id='su']")).getAttribute("value");
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		Assert.assertEquals(txtExpected, txtActual);
 	}
 
@@ -39,16 +39,16 @@ public class HelloWorld {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("test-type");
 
-		System.out.println("正在启动Chrome浏览器...");
+		System.out.println("---------->正在启动Chrome浏览器...");
 		System.setProperty("webdriver.chrome.driver", "files\\chromedriver.exe");
 		driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		System.out.println("启动Chrome浏览器完成！");
+		System.out.println("---------->启动Chrome浏览器完成！");
 
 		driver.manage().window().maximize();
 
 		driver.get("https://www.baidu.com");
-		System.out.println("加载指定页面完成！");
+		System.out.println("---------->加载指定页面完成！");
 	}
 	
 }
